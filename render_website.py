c9ca025be291b8cf3c8d77f848a6813b35d964e4
Rinat <swatlprus@gmail.com>
2023-03-31 16:42:57 +0500

    Used only single quotes

--- render_website.py
+++ render_website.py
@@ -26,14 +26,14 @@ def on_reload(library_books, number_book_on_page, columns_on_page):
         else:
             index_page = f'pages/index{number}.html'
 
-        with open(index_page, 'w', encoding="utf8") as file:
+        with open(index_page, 'w', encoding='utf8') as file:
             file.write(rendered_page)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     env = Env()
     env.read_env()
-    library_books = env("LIBRARY_BOOKS", default='media/books.json')
-    number_book_on_page = env("NUMBER_BOOKS_ON_PAGE", default=20)
-    columns_on_page = env("COLUMNS_ON_PAGE", default=2)
+    library_books = env('LIBRARY_BOOKS', default='media/books.json')
+    number_book_on_page = env('NUMBER_BOOKS_ON_PAGE', default=20)
+    columns_on_page = env('COLUMNS_ON_PAGE', default=2)
     on_reload(library_books, number_book_on_page, columns_on_page)
