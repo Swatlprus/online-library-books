2720a536e04340c533c504f7651b2568db2e1cd8
Rinat <swatlprus@gmail.com>
2023-03-31 15:45:02 +0500

    Change name variable

--- render_website.py
+++ render_website.py
@@ -7,8 +7,8 @@ from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 
 def on_reload(library_books):
-    with open(library_books, 'r', encoding='utf-8') as f:
-        books = json.load(f)
+    with open(library_books, 'r', encoding='utf-8') as file:
+        books_description = json.load(file)
 
     env = Environment(
         loader=FileSystemLoader('.'),
@@ -18,8 +18,8 @@ def on_reload(library_books):
     template = env.get_template('template.html')
     os.makedirs('./pages', exist_ok=True)
 
-    count_pages = math.ceil(len(books) / 20)
-    for number, book_page in enumerate(list(chunked(books, 20)), start=1):
+    count_pages = math.ceil(len(books_description) / 20)
+    for number, book_page in enumerate(list(chunked(books_description, 20)), start=1):
         rendered_page = template.render({'books': list(chunked(book_page, 2)), 'count_pages': count_pages, 'current_page': number})
         if number == 1:
             with open(f'pages/index.html', 'w', encoding="utf8") as file:
