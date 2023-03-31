0e128706bc667fe3243d15cbc67dce73552e7e5e
Rinat <swatlprus@gmail.com>
2023-03-31 15:47:12 +0500

    Change name variable pages_count

--- render_website.py
+++ render_website.py
@@ -18,9 +18,9 @@ def on_reload(library_books):
     template = env.get_template('template.html')
     os.makedirs('./pages', exist_ok=True)
 
-    count_pages = math.ceil(len(books_description) / 20)
+    pages_count = math.ceil(len(books_description) / 20)
     for number, book_page in enumerate(list(chunked(books_description, 20)), start=1):
-        rendered_page = template.render({'books': list(chunked(book_page, 2)), 'count_pages': count_pages, 'current_page': number})
+        rendered_page = template.render({'books': list(chunked(book_page, 2)), 'pages_count': pages_count, 'current_page': number})
         if number == 1:
             with open(f'pages/index.html', 'w', encoding="utf8") as file:
                 file.write(rendered_page)
